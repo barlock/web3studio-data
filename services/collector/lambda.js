c8188364f55371e7fb237bcfa601393da5924e18
Michael Barlock <barlockm@gmail.com>
2019-04-19 17:32:32 -0400

    Create lambda collector to run once a day

--- services/collector/lambda.js
+++ services/collector/lambda.js
@@ -1,8 +1,19 @@
-'use strict';
+const AWS = require('aws-sdk');
+const winston = require('winston');
+const collector = require('github-collector');
+const ElasticTransport = require('elastic-transport');
 
-module.exports.handler = async event => {
-  return {
-    message: 'Go Serverless v1.0! Your function executed successfully!',
-    event
-  };
+module.exports.handler = () => {
+  const elasticTransport = new ElasticTransport({
+    clientOpts: {
+      host: `https://${process.env.ELASTIC_ENDPOINT}`,
+      connectionClass: require('http-aws-es'),
+      awsConfig: new AWS.Config({ region: process.env.AWS_REGION })
+    },
+    transports: [new winston.transports.Console()]
+  });
+
+  collector(elasticTransport).subscribe({
+    error: console.log // eslint-disable-line no-console
+  });
 };
